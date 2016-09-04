2072cbf48b0a16e4aaeffbb108a08d63719caeb7
PhilLavoie <maidenphil@hotmail.com>
2016-09-03 20:16:24 -0400

    Put the RAML example in its own resource file.
    So that if we start having multiple test examples
    they don't clutter a class.

--- src/main/java/org/raml/sandbox/FunMain.java
+++ src/main/java/org/raml/sandbox/FunMain.java
@@ -13,26 +13,23 @@ import org.raml.v2.api.model.common.ValidationResult;
 import org.raml.v2.api.model.v10.api.Api;
 import org.raml.v2.api.model.v10.resources.Resource;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 /**
  * Created by ebeljea on 8/27/16.
  * Copyright Ericsson.
  */
 public class FunMain {
-    public static String raml1 =
-        "#%RAML 1.0 \n" + "title: Pet shop\n" + "version: 1\n" + "baseUri: /shop\n" + " \n"
-            + "/pets:\n" + "  description: this is it\n" + "  get:\n" + "    responses:\n"
-            + "      200:\n" + "        body:\n" + "          application/json:\n" + "  post:\n"
-            + "    body:\n" + "      application/json:\n" + "        type: object\n"
-            + "        properties:\n" + "          name: string\n" + "          kind: string\n"
-            + "          price: number\n" + " \n" + "  /{id}:\n" + "    put:\n" + "      body:\n"
-            + "        application/json:\n" + "    delete:\n" + "      responses:\n"
-            + "        400:";
+    public static void main(String[] args) throws URISyntaxException {
+        URL petshopRaml = FunMain.class.getResource("/petshop.raml");
 
-    public static void main(String[] args) {
+        RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(new File(petshopRaml.toURI()));
 
-
-
-        RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(raml1, ".");
         if (ramlModelResult.hasErrors()) {
             for (ValidationResult validationResult : ramlModelResult.getValidationResults()) {
                 System.out.println(validationResult.getMessage());
