a6ebc9e8afb0c9b4026635524d818cf60f4e8381
PhilLavoie <maidenphil@hotmail.com>
2016-09-05 13:05:29 -0400

    Configurating slf4j for tests
    - Using logback with a simple configuration.
    - Moved the sandbox to test package so that it can
    access the resources.
    - Scoped mockito dependency to test

--- src/test/java/org/raml/sandbox/FunMain.java
+++ src/test/java/org/raml/sandbox/FunMain.java
@@ -1,5 +1,6 @@
 package org.raml.sandbox;
 
+import com.google.common.base.Joiner;
 import org.raml.builders.Builders;
 import org.raml.builders.MethodBuilder;
 import org.raml.builders.ResourceBuilder;
@@ -7,21 +8,28 @@ import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
 import org.raml.v2.api.model.common.ValidationResult;
 import org.raml.v2.api.model.v10.api.Api;
+import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
 import org.raml.v2.api.model.v10.resources.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 
-/**
- * Created by ebeljea on 8/27/16.
- * Copyright Ericsson.
- */
 public class FunMain {
-    public static void main(String[] args) throws URISyntaxException {
-        URL petshopRaml = FunMain.class.getResource("/examples/petshop.raml");
+    private static final Logger logger = LoggerFactory.getLogger(FunMain.class);
 
-        RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(new File(petshopRaml.toURI()));
+    public static void main(String[] args) throws URISyntaxException {
+
+        if (args.length != 1) {
+            throw new IllegalArgumentException("usage: <resourceToParse>");
+        }
+
+        URL raml = FunMain.class.getResource(args[0]);
+
+        RamlModelResult ramlModelResult = new RamlModelBuilder().buildApi(new File(raml.toURI()));
 
         if (ramlModelResult.hasErrors()) {
             for (ValidationResult validationResult : ramlModelResult.getValidationResults()) {
@@ -29,11 +37,17 @@ public class FunMain {
             }
         } else {
             Api api = ramlModelResult.getApiV10();
-            System.err.println("displayName " + api.resources().get(0).description().value());
-            System.err.println("methods: " + api.resources().get(0).methods());
+
+            List<TypeDeclaration> typeDeclarations = api.types();
+
+            logger.info("type declarations: [{}]", Joiner.on(", ").join(typeDeclarations));
         }
 
 
+
+    }
+
+    private static void previousStuff() {
         ResourceBuilder resourceBuilder = Builders.resourceBuilder();
         MethodBuilder methodBuilder = Builders.methodBuilder();
         Resource r = resourceBuilder.withResourcePath("/baah").withDisplayName("sheep")
