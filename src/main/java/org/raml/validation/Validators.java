80978b4c224ca45b8c16193f84e5c4f08723db54
PhilLavoie <maidenphil@hotmail.com>
2016-09-10 17:36:30 -0400

    Finishing the remaining validators for raml v1.0

--- src/main/java/org/raml/validation/Validators.java
+++ src/main/java/org/raml/validation/Validators.java
@@ -6,27 +6,55 @@ import org.raml.v2.api.loader.ResourceLoader;
 import org.raml.v2.api.model.v10.RamlFragment;
 import org.raml.v2.api.model.v10.api.Api;
 import org.raml.v2.api.model.v10.api.Library;
+import org.raml.v2.api.model.v10.datamodel.ExampleSpec;
+import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
 import org.raml.v2.api.model.v10.methods.Trait;
+import org.raml.v2.api.model.v10.resources.ResourceType;
+import org.raml.v2.api.model.v10.security.SecurityScheme;
 import org.raml.yagi.framework.nodes.Node;
 
 public class Validators {
 
     //Prevent construction.
-    private Validators() {}
+    private Validators() {
+    }
 
     public static RamlValidator forApi(Api api, ResourceLoader resourceLoader) {
-        Node rootNode = NodeExtractor.extractNodeFromProxy(api);
-        return RamlV10Validator.create(rootNode, resourceLoader, RamlFragment.Default);
+        return forAnyProxy(api, resourceLoader, RamlFragment.Default);
     }
 
     public static RamlValidator forTrait(Trait trait, ResourceLoader resourceLoader) {
-        Node rootNode = NodeExtractor.extractNodeFromProxy(trait);
-        return RamlV10Validator.create(rootNode, resourceLoader, RamlFragment.Trait);
+        return forAnyProxy(trait, resourceLoader, RamlFragment.Trait);
     }
 
     public static RamlValidator forLibrary(Library library, ResourceLoader resourceLoader) {
-        Node rootNode = NodeExtractor.extractNodeFromProxy(library);
-        return RamlV10Validator.create(rootNode, resourceLoader, RamlFragment.Library);
+        return forAnyProxy(library, resourceLoader, RamlFragment.Library);
+    }
+
+    private static RamlValidator forTypeDeclaration(TypeDeclaration typeDeclaration,
+        ResourceLoader resourceLoader) {
+        return forAnyProxy(typeDeclaration, resourceLoader, RamlFragment.DataType);
+    }
+
+    private static RamlValidator forNamedExample(ExampleSpec exampleSpec,
+        ResourceLoader resourceLoader) {
+        return forAnyProxy(exampleSpec, resourceLoader, RamlFragment.NamedExample);
+    }
+
+    private static RamlValidator forAnyProxy(Object exampleSpec, ResourceLoader resourceLoader,
+        RamlFragment dataType) {
+        Node rootNode = NodeExtractor.extractNodeFromProxy(exampleSpec);
+        return RamlV10Validator.create(rootNode, resourceLoader, dataType);
+    }
+
+    private static RamlValidator forResourceType(ResourceType resourceType,
+        ResourceLoader resourceLoader) {
+        return forAnyProxy(resourceType, resourceLoader, RamlFragment.ResourceType);
+    }
+
+    private static RamlValidator forSecurityScheme(SecurityScheme securityScheme,
+        ResourceLoader resourceLoader) {
+        return forAnyProxy(securityScheme, resourceLoader, RamlFragment.SecurityScheme);
     }
 
     public static RamlValidator forResult(RamlModelResult ramlModelResult,
@@ -45,14 +73,19 @@ public class Validators {
                 return forTrait(ramlModelResult.getTrait(), resourceLoader);
             case Library:
                 return forLibrary(ramlModelResult.getLibrary(), resourceLoader);
-            case DocumentationItem:
             case DataType:
+                return forTypeDeclaration(ramlModelResult.getTypeDeclaration(), resourceLoader);
             case NamedExample:
+                return forNamedExample(ramlModelResult.getExampleSpec(), resourceLoader);
             case ResourceType:
+                return forResourceType(ramlModelResult.getResourceType(), resourceLoader);
+            case SecurityScheme:
+                return forSecurityScheme(ramlModelResult.getSecurityScheme(), resourceLoader);
+            //Unsupported yet in the parser
+            case DocumentationItem:
             case AnnotationTypeDeclaration:
             case Overlay:
             case Extension:
-            case SecurityScheme:
             default:
                 throw new UnsupportedOperationException("fragment not yet supported: " + fragment);
         }
